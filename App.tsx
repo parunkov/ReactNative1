b5f09c4ed16ba4f84dc2e9376fd6ae3eea2234ab
parunkov <parunkov@gmail.com>
2023-06-13 14:58:38 +0500

    list creating

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import data from './assets/data.json';
+  
+console.log(data);
 
 export default function App() {
+  useEffect(() => {
+    console.log(2222);
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Список ТС</Text>
       <StatusBar style="auto" />
+      {data.map((item, key) => 
+        <View style={styles.auto} key={key} >
+          <Text>{item.name}</Text>
+          <Text>{item.driver}</Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -15,6 +29,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  auto: {
+    width: '90%',
+    borderColor: 'red',
+    borderWidth: 1,
+  }
 });
